61e224394164d67d906b3e8e1c7c9647088e8744
Bojan <bdimosky35@yahoo.com>
2024-09-28 06:59:02 +0200

    update hero image

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
                     <div className='absolute -top-32 left-[calc(50%-340px)] w-[1230px]
                      pointer-events-none hero-img_res'>
                         <img
-                            src="/images/hero.png"
+                            src="/images/hero.webp"
                             alt="hero"
                             className='size-1230 max-lg:h-auto'/>
                     </div>
